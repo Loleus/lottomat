9e40000e73cf78d049a3d30fa8a4388ef586b3a5
Lukasz Kaminski <44691382+Loleus@users.noreply.github.com>
2021-09-22 21:38:47 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -8,60 +8,73 @@ const listBtn = document.getElementById("pokaz");
 const aside = document.querySelector('aside');
 const list = document.querySelector('aside.list');
 const listWRP = document.querySelector('aside.list div');
+
 const sortYourNumbers = () => {
-    result.sort(function (a, b) {
-        return a - b
-    });
+    result.sort((a, b) => a - b);
     results.push(result);
     result = [];
 }
+const end = () => {
+    setTimeout(() => {
+        mainBtn.removeEventListener("click", handleDraw);
+        mainBtn.addEventListener('click', reset)
+        listBtn.disabled = false;
+        mainBtn.disabled = false;
+        mainBtn.classList.remove("dots");
+        mainBtn.innerText = "Zapisz";
+        for (const item of balls) {
+            if (!item.classList.contains('selected')) {
+                item.style.opacity = 0.4;
+            }
+        }
+    }, 666)
+}
 const unselBalls = () => {
-    for (let i = 0; i < all.length; i++) {
-        all[i].classList.contains('selected') ?
-            all[i].classList.remove('selected') :
-            all[i].style.opacity = 1;
+    for (const item of balls) {
+        item.classList.remove('selected');
+        item.style.opacity = 1;
+    }
+}
+const getNumber = () => {
+    let rndNumber = Math.floor(Math.random() * 49 + 1);
+    for (elem of result) {
+        if (rndNumber === elem) {
+            return getNumber();
+        }
+    }
+    return rndNumber
+}
+const showInTimeout = (newNumber, i) => {
+    setTimeout(() => {
+        renderBalls(newNumber);
+        if (i ===5) {
+            return end();
+        }
+    }, 666* (i + 1))
+}
+const fillArray = () => {
+    for (let i = 0; i < 6; i++) {
+        let newNumber = getNumber();
+        result.push(newNumber);
+        showInTimeout(newNumber, i)
     }
 }
 const handleDraw = () => {
     listBtn.style.opacity = 0.4;
-    listBtn.removeEventListener("click", resultsList, false)
+    listBtn.disabled = true;
+    mainBtn.disabled = true;
     if ($samples <= $collect) {
-        mainBtn.removeEventListener("click", handleDraw);
         mainBtn.innerText = "l o s u j ę"
         mainBtn.classList.add("dots");
-        drawing()
+        fillArray();
     } else {
         console.log('za mała pula')
     }
 }
-const drawing = () => {
-    const wynikLosowania = Math.floor(Math.random() * $collect + 1);
-    for (let i = 0; i < result.length; i++) {
-        if (wynikLosowania === result[i]) {
-            return drawing()
-        }
-    }
-    let id = "ball" + wynikLosowania
-    let wylosowana = document.getElementById(id);
-    setTimeout(() => {
-        wylosowana.classList.add('selected')
-        if (result.length === $samples) {
-            setTimeout(() => {
-                mainBtn.classList.remove("dots");
-                mainBtn.innerText = "Zapisz";
-                mainBtn.style.color = "white";
-                for (let i = 0; i < all.length; i++) {
-                    !all[i].classList.contains('selected') ?
-                        all[i].style.opacity = 0.45 :
-                        null;
-                }
-            }, 666)
-            mainBtn.addEventListener("click", reset)
-            return
-        }
-        drawing()
-    }, 666);
-    result.push(wynikLosowania);
+const renderBalls = (wynikLosowania) => {
+    const id = "ball" + wynikLosowania
+    const wylosowana = document.getElementById(id);
+        wylosowana.classList.add('selected') // color selected ball
 }
 const reset = () => {
     sortYourNumbers();
@@ -72,9 +85,15 @@ const reset = () => {
 const resultsList = () => {
     if (results.length > 0) {
         list.classList.toggle('open')
-        list.classList.contains('open') ?
-            listBtn.innerText = "Zamknij listę" :
+        if (list.classList.contains('open')) {
+            listBtn.innerText = "Zamknij listę"
+            mainBtn.disabled = true;
+            mainBtn.style.opacity = 0.4;
+        } else {
             listBtn.innerText = "Lista po " + results.length + " losowaniu";
+            mainBtn.disabled = false;
+            mainBtn.style.opacity = 1;
+        }
     } else {
         listBtn.innerText = "Nie losowałeś!";
     }
@@ -84,8 +103,8 @@ const updateInfo = () => {
     const infoTxtH1 = ` ${results[results.length - 1]} `
     aside.innerText = "Jeszcze nie losowałeś";
     if (results.length > 0) {
-        aside.innerText = infoTxt + infoTxtH1;
-        listWRP.innerHTML += `<p>${infoTxt}<h1>${infoTxtH1}</h1></p><br>`
+        aside.innerText = "Ostatnie losowanie: " + infoTxtH1;
+        listWRP.innerHTML += `<p>${infoTxt}<h1>${infoTxtH1}</h1></p>`
     }
 }
 const resetBtn = () => {
@@ -104,13 +123,14 @@ const resetBtn = () => {
 const ballGenerator = () => {
     for (let i = 1; i < $collect + 1; i++) {
         const ball = document.createElement("div");
+        const id = "ball" + i;
         ball.textContent = i;
         ball.setAttribute("class", "kulki")
-        ball.setAttribute("id", "ball" + i)
+        ball.setAttribute("id", id)
         ballsWrapper.appendChild(ball);
     }
 }
 ballGenerator();
 resetBtn();
 updateInfo();
-const all = document.querySelectorAll('.kulki');
+const balls = document.querySelectorAll('.kulki');
